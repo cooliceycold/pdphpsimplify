87234f5c4dc0bc7c51f725a2348c42cf891ae831
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2023-12-13 08:13:00 +0800

    Add files via upload

---
+++ static/ready.js
@@ -0,0 +1,50 @@
+// add qrcode
+$('[data-qrcode-attr]').each(function (i) {
+    this.outerHTML = this.outerHTML + `<span class="qrcode" id="TEMP_QRCODE_ATTR_ID${i}"></span>`;
+    const codeDiv = document.querySelector(`#TEMP_QRCODE_ATTR_ID${i}`),
+        size = (this.getAttribute('data-qrcode-size') !== null) ? this.dataset.qrcodeSize : 512;
+    let level = QRCode.CorrectLevel.M;
+    if (this.getAttribute('data-qrcode-level') !== null) {
+        switch (this.dataset.qrcodeLevel) {
+            case 'L':
+                level = QRCode.CorrectLevel.L;
+                break;
+            case 'M':
+                break;
+            case 'H':
+                level = QRCode.CorrectLevel.H;
+                break;
+            case 'Q':
+                level = QRCode.CorrectLevel.Q;
+                break;
+            default:
+        }
+    }
+    makeQRCode(codeDiv, this.getAttribute(this.dataset.qrcodeAttr), size, level);
+    codeDiv.removeAttribute('id');
+});
+
+$('[data-qrcode-text]').each(function (i) {
+    this.outerHTML = this.outerHTML + `<span class="qrcode" id="TEMP_QRCODE_TEXT_ID${i}"></span>`;
+    const codeDiv = document.querySelector(`#TEMP_QRCODE_TEXT_ID${i}`),
+        size = (this.getAttribute('data-qrcode-size') !== null) ? this.dataset.qrcodeSize : 512;
+    let level = QRCode.CorrectLevel.M;
+    if (this.getAttribute('data-qrcode-level') !== null) {
+        switch (this.dataset.qrcodeLevel) {
+            case 'L':
+                level = QRCode.CorrectLevel.L;
+                break;
+            case 'M':
+                break;
+            case 'H':
+                level = QRCode.CorrectLevel.H;
+                break;
+            case 'Q':
+                level = QRCode.CorrectLevel.Q;
+                break;
+            default:
+        }
+    }
+    makeQRCode(codeDiv, this.dataset.qrcodeText, size, level);
+    codeDiv.removeAttribute('id');
+});
